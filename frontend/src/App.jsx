ea422a16fa4ee751f2c028a58dd5e3dfc6734d28
Abhi Mishra <sonusamda@gmail.com>
2024-09-21 22:10:21 +0530

    Layout route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import Navbar from "./components/Navbar.jsx";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx"
+import { Outlet } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
 
 
 function App() {
   return (
     <>
-    
-      <Navbar />
       <Routes>
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn/>} />
+        <Route element={<Layout />}>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Route>
       </Routes>
     </>
   );
